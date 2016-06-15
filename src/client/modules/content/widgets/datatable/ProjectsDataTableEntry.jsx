541dc1aec41d7234ad2ef19b1e1162f3c616bba5
Patrick Pei <patrickkerrypei@users.noreply.github.com>
2016-06-15 17:16:59 -0500

    Implemented time ago for table popovers (#15)

--- src/client/modules/content/widgets/datatable/ProjectsDataTableEntry.jsx
+++ src/client/modules/content/widgets/datatable/ProjectsDataTableEntry.jsx
@@ -6,8 +6,10 @@
 // Libraries
 import React from 'react/lib/React';
 import Link from 'react-router/lib/Link';
+import {OverlayTrigger, Popover} from 'react-bootstrap';
 // Self-defined
 import DataTableEntry from './DataTableEntry.jsx';
+import {timeAgo} from '../../../../utils/utils';
 
 export default class ProjectsDataTableEntry extends React.Component {
 
@@ -16,11 +18,33 @@ export default class ProjectsDataTableEntry extends React.Component {
 
         return <DataTableEntry>
             <td>{this.props.owner}</td>
+
             <td className="sorting_1">
                 <Link to={buildLink}>{this.props.name}</Link>
             </td>
-            <td>{this.props.info.viewedAt}</td>
-            <td>{this.props.info.modifiedAt}</td>
+
+            <td>
+                <OverlayTrigger trigger={["hover", "focus"]} placement="top" overlay={
+                        <Popover title="Last Viewed At:" id="Viewed At">
+                            {new Date(this.props.info.viewedAt).toString()}
+                            <br/><br/>
+                            <i>{`Viewed by: ${this.props.info.viewer}`}</i>
+                        </Popover>}>
+                    <i>{timeAgo(this.props.info.viewedAt)}</i>
+                </OverlayTrigger>
+            </td>
+
+            <td>
+                <OverlayTrigger trigger={["hover", "focus"]} placement="top" overlay={
+                        <Popover title="Last Viewed At:" id="Viewed At">
+                            {new Date(this.props.info.modifiedAt).toString()}
+                            <br/><br/>
+                            <i>{`Modified by: ${this.props.info.modifier}`}</i>
+                        </Popover>}>
+                    <i>{timeAgo(this.props.info.modifiedAt)}</i>
+                </OverlayTrigger>
+            </td>
+
         </DataTableEntry>;
     }
 }
