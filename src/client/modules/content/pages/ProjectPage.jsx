122af383453b0e1fec821e1ab3e15352214e226e
peip <patrickkerry.pei@gmail.com>
2016-07-04 14:43:44 -0500

    Open in editor opens in the current tab, user can choose to ctrl+click

--- src/client/modules/content/pages/ProjectPage.jsx
+++ src/client/modules/content/pages/ProjectPage.jsx
@@ -41,8 +41,7 @@ export default class ProjectPage extends React.Component {
                             <i className="fa fa-cube"/>{` ${this.props.params.ownerId} / ${this.props.params.projectName}`}
                         </h2>
                         <div className="col-md-2" style={{paddingRight: "30px", paddingTop: "14px"}}>
-                            <a href={"/?project=" + window.encodeURIComponent(`${this.props.params.ownerId}+${this.props.params.projectName}`)}
-                               target="_blank">
+                            <a href={"/?project=" + window.encodeURIComponent(`${this.props.params.ownerId}+${this.props.params.projectName}`)}>
                                 <Button bsStyle="primary" style={{float: "right"}}>
                                     View in editor
                                 </Button>
