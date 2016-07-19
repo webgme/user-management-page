92ee19cfddff4216c977e8cb96c094538ed214f2
peip <patrickkerry.pei@gmail.com>
2016-07-19 11:58:45 -0500

    Restructure header/sidebar folders

---
+++ src/common/components/sidebar/SideBarUserPanel.jsx
@@ -0,0 +1,53 @@
+/* global window */
+
+/**
+ * Sidebar user panel container
+ * @author patrickkerrypei / https://github.com/patrickkerrypei
+ */
+
+// Libraries
+import React, { Component, PropTypes } from 'react';
+import { browserHistory } from 'react-router';
+// Self-defined
+import { fetchUserIfNeeded } from '../../actions/user';
+// Style
+import { SideBarUserPanel as STYLE } from '../../../client/style';
+
+export default class SideBarUserPanel extends Component {
+
+    componentDidMount() {
+        const { dispatch } = this.props;
+
+        dispatch(fetchUserIfNeeded());
+    }
+
+    goToEditor() {
+        browserHistory.push('/');
+        window.location.reload();
+    }
+
+    render() {
+        const { user } = this.props;
+
+        return (
+            <div className="user-panel" style={STYLE.panel}>
+                <div className="pull-left image" style={{cursor: "pointer"}}>
+                    <img alt="User Image"
+                         className="img-circle"
+                         onClick={this.goToEditor}
+                         src="/img/gme-logo.png"
+                         style={STYLE.imageIcon}/>
+                </div>
+                <div className="pull-left info">
+                    <p style={STYLE.name}>&nbsp;{user._id}&nbsp;</p>
+                    <span style={STYLE.status}><i className="fa fa-circle text-success"/> Online</span>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+SideBarUserPanel.propTypes = {
+    user: PropTypes.object.isRequired
+};
--- src/common/containers/header/navigation_bar/UserMenu.jsx
+++ src/common/containers/header/navigation_bar/UserMenu.jsx
@@ -4,53 +4,9 @@
  */
 
 // Libraries
-import React, { Component, PropTypes } from 'react';
-import { Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 // Self-defined
-import { fetchUserIfNeeded } from '../../../actions/user';
-import { UserMenu as STYLE } from '../../../../client/style';
-
-class UserMenu extends Component {
-
-    componentDidMount() {
-        const { dispatch } = this.props;
-        dispatch(fetchUserIfNeeded());
-    }
-
-    render() {
-        const { user } = this.props;
-
-        return (
-            <li className="dropdown user user-menu">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-                    <img src="/img/gme-logo.png" className="user-image" alt="User Image"/>
-                    <span className="hidden-xs">&nbsp;{user._id}&nbsp;</span>
-                </a>
-                <ul className="dropdown-menu">
-                    <li className="user-header" style={STYLE.userHeader}>
-                        <img src="/img/gme-logo.png" className="img-circle" alt="User Image"/>
-                        <p>
-                            {user._id}
-                        </p>
-                    </li>
-                    <li className="user-footer" style={STYLE.logoutAreaBorder}>
-                        <div className="col-xs-4 text-center" style={STYLE.logoutButtonPadding}>
-                            <a href="/logout">
-                                <Button style={STYLE.logoutButtonBorder}>Sign Out</Button>
-                            </a>
-                        </div>
-                    </li>
-                </ul>
-            </li>
-        );
-    }
-
-}
-
-UserMenu.propTypes = {
-    user: PropTypes.object.isRequired
-};
+import UserMenu from '../../../components/header/navigation_bar/UserMenu';
 
 const mapStateToProps = (state) => {
     const { user } = state.user;
