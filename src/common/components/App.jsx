1c08dbb29d1eb3302e4e9c9aa709791b0201c471
Tamas Kecskes <tamas.kecskes@vanderbilt.edu>
2018-06-15 18:23:41 -0500

    #83 faulty base path

--- src/common/components/App.jsx
+++ src/common/components/App.jsx
@@ -28,7 +28,7 @@ export default class App extends Component {
 
     constructor(props) {
         super(props);
-        this.restClient = new RestClient(document.getElementById('baseUrlHolder').getAttribute('data') + 'api/');
+        this.restClient = new RestClient(document.getElementById('baseUrlHolder').getAttribute('data') + '/api/');
         // Set the referrer in the session store (if not already set)
         if (window.top === window && typeof window.sessionStorage.getItem('originalReferrer') !== 'string') {
             window.sessionStorage.setItem('originalReferrer', window.document.referrer);
